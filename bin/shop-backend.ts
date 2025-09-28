33ae521e6da0d8756322fb85477c019cf42b132f
Juraj Pejnovic <juraj.pejnovic@miqdigital.com>
2025-09-29 01:40:11 +0200

    refactor: add hellolambda to deployment

--- bin/shop-backend.ts
+++ bin/shop-backend.ts
@@ -2,8 +2,10 @@
 import * as cdk from "aws-cdk-lib";
 import { ProductServiceStack } from "../lib/product-service/product-service-stack";
 import { TodoStack } from "../lib/todo/TodoStack";
+import { HelloLambdaStack } from "../lib/hello-lambda/hello-lamba-stack";
 
 const app = new cdk.App();
 
+new HelloLambdaStack(app, "HelloLambdaStack", {});
 new ProductServiceStack(app, "ProductLambdaStack", {});
 new TodoStack(app, "TodoStack", {});
