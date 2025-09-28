5432a6ba2e9d02be3cdb5bfab6839c047d1a830d
Juraj Pejnovic <juraj.pejnovic@miqdigital.com>
2025-09-28 02:19:09 +0200

    feat: add todostack and make it work

--- bin/shop-backend.ts
+++ bin/shop-backend.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { ProductServiceStack } from "../lib/product-service/product-service-stack";
+import { TodoStack } from "../lib/todo/TodoStack";
 
 const app = new cdk.App();
 
 new ProductServiceStack(app, "ProductLambdaStack", {});
+new TodoStack(app, "TodoStack", {});
